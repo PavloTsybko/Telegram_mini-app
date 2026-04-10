28bcfd054846607685af030a35f87084ffcb644f
PavloTsybko <pavel.tsybko@gmail.com>
2026-04-11 00:00:08 +0300

    Update main.tsx with TonConnectProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
+import { TonConnectProvider } from '@tonconnect/ui-react'
 import WebApp from '@twa-dev/sdk'
 import './index.css'
 
+// Initialize Telegram WebApp
 WebApp.ready();
+
+const manifestUrl = 'https://bmnumbers.space/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TonConnectProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectProvider>
   </React.StrictMode>,
 )
