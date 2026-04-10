858c42ef4e5ae0c626cc56f10748f37a6750ee5e
PavloTsybko <pavel.tsybko@gmail.com>
2026-04-11 00:02:26 +0300

    Update vite config for Telegram

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
+  base: './',
   build: {
-    outDir: 'dist'
+    outDir: 'docs',
+    emptyOutDir: true,
   },
   server: {
-    port: 3000,
-    https: true
+    host: '0.0.0.0',
+    port: 5173,
   }
-});
+})
